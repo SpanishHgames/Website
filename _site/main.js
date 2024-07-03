687f2645a322f80596f37734ee9b678802b810cb
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-03 17:36:59 -0600

    update

--- _site/main.js
+++ _site/main.js
@@ -28,3 +28,28 @@ console.log(`
 
 
 // what are you looking at here?
+
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById('modal-img');
+
+const img1 = document.getElementById('img-1');
+const img2 = document.getElementById('img-2');
+
+function openModal(imgSrc) {
+    modalImg.src = imgSrc;
+    modal.classList.remove('hidden');
+}
+
+function closeModal() {
+    modal.classList.add('hidden');
+}
+
+img1.addEventListener('click', function() {
+    openModal('https://avatars.githubusercontent.com/u/107765373?v=4');
+});
+
+img2.addEventListener('click', function() {
+    openModal('https://avatars.githubusercontent.com/u/149289426?v=4');
+});
+
+document.querySelector('.modal-close').addEventListener('click', closeModal);
